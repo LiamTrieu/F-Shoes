aeafc2f2a74b8745365d14744c99fee91e127637
kienvan131203 <kienvan131203@gmail.com>
2023-09-02 22:16:17 +0700

    80% giao diện màn khách hàng, update KhuyenMaiController,KhuyenMaiRespone

--- back_end/src/main/java/com/fshoes/core/admin/khuyenmai/repository/KhuyenMaiRepository.java
+++ back_end/src/main/java/com/fshoes/core/admin/khuyenmai/repository/KhuyenMaiRepository.java
@@ -1,8 +1,15 @@
 package com.fshoes.core.admin.khuyenmai.repository;
 
+import com.fshoes.core.admin.khuyenmai.model.respone.KhuyenMaiRespone;
 import com.fshoes.repository.PromotionRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface KhuyenMaiRepository extends PromotionRepository {
+    @Query(value = "select Name, time_start as TimeStart, time_end as TimeEnd, Value, Status from Promotion", nativeQuery = true)
+    List<KhuyenMaiRespone> getAllKhuyenMai();
+
 }
