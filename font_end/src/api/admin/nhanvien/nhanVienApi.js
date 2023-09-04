b24e4ee035454f0389d12abb280a529ee3c9904f
PHANTHIQUYNHTRA\HP <trangptqph26218@fpt.edu.vn>
2023-09-04 12:37:48 +0700

    call api staff

--- font_end/src/api/admin/nhanvien/nhanVienApi.js
+++ font_end/src/api/admin/nhanvien/nhanVienApi.js
@@ -1,4 +1,28 @@
 import axios from "axios";
-const url = "http://localhost:8080";
+const urlStaff = "http://localhost:8080";
 
-
+const staffApi = {
+  getAllStaff: () => {
+    const urlGetAll = `${urlStaff}/api/staff/find-all`;
+    return axios.get(urlGetAll);
+  },
+  searchAndGetPageStaff: (page, searchTen) => {
+    const urlSearchByInput = `${urlStaff}/api/staff/search-getPage?page=${page}&searchTen=${searchTen}`;
+    return axios.get(urlSearchByInput);
+  },
+  addStaff: (addStaffRequest) => {
+    const urlAddStaff = {
+      url: `${urlStaff}/api/staff/add`,
+      body: addStaffRequest,
+    };
+    return axios.get(urlAddStaff);
+  },
+  updateStaff: (id, addStaffRequest) => {
+    const urlUpdateStaff = {
+      url: `${urlStaff}/api/staff/update/${id}`,
+      body: addStaffRequest,
+    };
+    return axios.get(urlUpdateStaff);
+  },
+};
+export default staffApi;
