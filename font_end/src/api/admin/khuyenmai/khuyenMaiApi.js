f7f0000ce2987422ee855be3373bad92fa4849fa
kienvan131203 <kienvan131203@gmail.com>
2023-09-04 19:29:11 +0700

    fill table, addPromotion, detailPromotion (chưa xử lý date)

--- font_end/src/api/admin/khuyenmai/khuyenMaiApi.js
+++ font_end/src/api/admin/khuyenmai/khuyenMaiApi.js
@@ -1,10 +1,25 @@
-// import axios from "axios";
-// const url = "http://localhost:8080";
-// const khuyenMaiApi = {
-//   getAll: () => {
-//     const GetAll = `${url}/api/promotion/get-all`;
-//     return axios.get(GetAll);
-//   },
-// };
+import axios from "axios";
+const urlPromotion = "http://localhost:8080";
+const khuyenMaiApi = {
+  getAll: () => {
+    const GetAll = `${urlPromotion}/api/promotion/get-all`;
+    return axios.get(GetAll);
+  },
 
-// export default khuyenMaiApi;
+  getPage: (totalPages) => {
+    const urlPage = `${urlPromotion}/api/promotion/page?page=${totalPages}`;
+    return axios.get(urlPage);
+  },
+
+  getById: (id) => {
+    const getPageById = `${urlPromotion}/api/promotion/get-one/${id}`;
+    return axios.get(getPageById);
+  },
+
+  addPromotion: (Promotion) => {
+    const urlAddPromotion = `${urlPromotion}/api/promotion/add`;
+    return axios.post(urlAddPromotion, Promotion);
+  },
+};
+
+export default khuyenMaiApi;
--- font_end/src/routers/adminroute/adpromotionroute.jsx
+++ font_end/src/routers/adminroute/adpromotionroute.jsx
@@ -1,9 +1,11 @@
 import AdPromotionAdd from "../../pages/admin/khuyenmai/AdPromotionAdd";
 import AdPromotionPage from "../../pages/admin/khuyenmai/AdPromotionPage";
+import AdPromotionDetail from "../../pages/admin/khuyenmai/AdpromotionDetail";
 
 const adPromotionRoute = [
   { path: "/admin/promotion", element: <AdPromotionPage /> },
   { path: "/admin/promotion/add", element: <AdPromotionAdd /> },
+  { path: "/admin/promotion/get-one/:id", element: <AdPromotionDetail /> },
 ];
 
 export default adPromotionRoute;
