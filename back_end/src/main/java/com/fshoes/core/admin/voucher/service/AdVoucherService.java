88b43bcb6658f6082c85a9c66ae65d053639a923
Tên tk github <tuannvph25577@fpt.edu.vn>
2023-09-06 22:33:46 +0700

    xử lý được ngày ở màn thêm voucher

--- back_end/src/main/java/com/fshoes/core/admin/voucher/service/AdVoucherService.java
+++ back_end/src/main/java/com/fshoes/core/admin/voucher/service/AdVoucherService.java
@@ -3,23 +3,22 @@ package com.fshoes.core.admin.voucher.service;
 import com.fshoes.core.admin.voucher.model.request.AdVoucherRequest;
 import com.fshoes.core.admin.voucher.model.request.AdVoucherSearch;
 import com.fshoes.core.admin.voucher.model.respone.AdVoucherRespone;
-import com.fshoes.core.common.PageableRequest;
 import com.fshoes.entity.Voucher;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 
+import java.text.ParseException;
 import java.util.List;
 
 public interface AdVoucherService {
     List<Voucher> getAllVoucher();
 
-    Voucher getVoucherById(Integer id);
+    AdVoucherRespone getVoucherById(Integer id);
 
     Page<Voucher> getPageVoucher(Integer page);
 
     Voucher addVoucher(AdVoucherRequest voucherRequest);
 
-    Boolean updateVoucher(Integer id, AdVoucherRequest voucherRequest);
+    Boolean updateVoucher(Integer id, AdVoucherRequest voucherRequest) throws ParseException;
 
     Boolean deleteVoucher(Integer id);
 
